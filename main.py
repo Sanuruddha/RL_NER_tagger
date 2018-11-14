1120c26f08497c873501bf4e8c8c06d187fbd22c
shihan <shihananuruddha@gmail.com>
2018-11-14 11:36:01 +0530

    fixes get input being called twice in main

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from model import Learner
 learner = Learner()
 learner.get_input('train.txt')
 #learner.split_train_test(90)
-learner.get_input('test.txt')
+learner.get_test_input('test.txt')
 learner.learn(0.9)
 learner.test_classifier()
