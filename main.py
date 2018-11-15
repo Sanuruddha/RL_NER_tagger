7b19cf382582459ff7220432ff99c4beadc81816
shihan <shihananuruddha@gmail.com>
2018-11-15 20:35:04 +0530

    adds while true in main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from model import Learner
 
-learner = Learner()
-learner.get_input('train.txt')
-#learner.split_train_test(90)
-learner.get_test_input('test.txt')
-learner.learn(0.9)
-learner.test_classifier()
+while True:
+    learner = Learner()
+    learner.get_input('train.txt')
+    #learner.split_train_test(90)
+    learner.get_test_input('test.txt')
+    learner.learn(0.9)
+    learner.test_classifier()
